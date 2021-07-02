9cdab27fb679e8a6502bd0341208d7ac4ab3c9b2
Jeff Scott Brown <brownj@objectcomputing.com>
2021-07-02 10:07:29 -0500

    https://stackoverflow.com/questions/68218178/how-do-i-force-all-constructor-args-be-specified

---
+++ app/src/main/groovy/daggett/MyClass.groovy
@@ -0,0 +1,12 @@
+package daggett
+
+class MyClass {
+    String paramOne
+    String paramTwo
+
+    MyClass(paramOne, paramTwo) throws Exception {
+        this.paramOne = paramOne
+        this.paramTwo = paramTwo
+    }
+
+}
